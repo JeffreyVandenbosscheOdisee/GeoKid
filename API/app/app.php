3bc0a2a1cde86290dd3f3a2082fc2401720a4144
Jeffrey Vandenbossche <jeffrey.vandenbossche@gmail.com>
2016-05-29 14:20:51 +0200

    test CORS

--- API/app/app.php
+++ API/app/app.php
@@ -53,7 +53,7 @@ $app->before(function(Request $request)
 // CORS response
 $app->after(function(Request $request, Response $response) 
 {
-   $response->headers->set("Access-Control-Allow-Origin","*");
+   // $response->headers->set("Access-Control-Allow-Origin","*");
    $response->headers->set("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
 });
 
